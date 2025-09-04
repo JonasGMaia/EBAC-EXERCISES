fc78a2c9ca275e045755ee9a1bb244b7e677294f
Jonas Maia <jonasgmaia2@gmail.com>
2025-09-04 14:29:32 -0300

    Add files via upload

---
+++ exercicio_ts.ts
@@ -0,0 +1,6 @@
+const multiplicacao = (primeiro:number, segundo:number):number => primeiro*segundo;
+const saudacao = (nome:string):string => "Olá "+ nome;
+const usuario = saudacao("Charlie");
+const result = multiplicacao(5,4);
+console.log(result)
+console.log(usuario)
